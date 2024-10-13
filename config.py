3d451a660c7433fdf8f31a28a8ddf20061457a44
Bernhard Rapp <boerny@linuxtage.at>
2024-10-13 16:08:38 +0200

    chores: early exit on invalid config, use constant

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 """ Configuration file, set your settings here"""
 
+import sys
+
 ### configure here
 # your language
 LANG = 'de'
@@ -15,19 +17,25 @@ OPERATION = 'Save+draft'
 
 
 ### do not edit below
+INVALID_CONFIG = 1
+
 languages_supported = ['en', 'de', 'nl', 'fr']
 
 operations_supported = ['Save+draft', 'Complete+repair']
 
 ### check config
 if LANG not in languages_supported:
-    print("language has to be one of: " + ','.join(languages_supported))
+    print("Invalid config: Language has to be one of: " + ','.join(languages_supported))
+    sys.exit(INVALID_CONFIG)
 
 if OPERATION not in operations_supported:
-    print("Following operations allowed only: " + ','.join(languages_supported))
+    print("Invalid config: Following operations allowed only: " + ','.join(languages_supported))
+    sys.exit(INVALID_CONFIG)
+
 
 if EXCEL_SKIP_HEADER <= 0:
-    print("EXCEL_SKIP_HEADER requires to be an positiv number. 1 means no rows are skipped. Unless you really don't require any mapping you always need to skip at least one row!")
+    print("Invalid config: EXCEL_SKIP_HEADER requires to be an positiv number. 1 means no rows are skipped. Unless you really don't require any mapping you always need to skip at least one row!")
+    sys.exit(INVALID_CONFIG)
 
 
 def get_lang_specific(lang):
